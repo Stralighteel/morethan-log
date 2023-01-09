1d89ca4f8860bc6e463e630b5fa85e28882091af
Stralighteel <stralighteel@gmail.com>
2023-01-09 19:41:06 +0900

    fix: no-image author
    이미지 없는 작성자의 경우 이미지 출력을 하지 않게 변경

--- src/containers/PostDetail/components/PostHeader.tsx
+++ src/containers/PostDetail/components/PostHeader.tsx
@@ -21,14 +21,16 @@ const PostHeader: React.FC<Props> = ({ data }) => {
           <div className="flex items-center gap-4 mb-3">
             {data.author && data.author[0] && (
               <div className="flex items-center gap-2">
-                <Image
-                  className="rounded-full"
-                  src={data.author[0].profile_photo}
-                  alt="profile_photo"
-                  loader={imageLoader}
-                  width={24}
-                  height={24}
-                />
+                {ata.author[0]?.profile_photo ? (<Image
+                    className="rounded-full"
+                    src={data.author[0].profile_photo}
+                    alt="profile_photo"
+                    loader={imageLoader}
+                    width={24}
+                    height={24}
+                  />)
+                  : null
+                }
                 <div className="">
                   {`${data.author[0].last_name}${data.author[0].first_name}`}
                 </div>
