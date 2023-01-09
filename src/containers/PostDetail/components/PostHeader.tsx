5e1c5d725f25dcf074926472619912ef83bfa353
Stralighteel <stralighteel@gmail.com>
2023-01-09 19:54:01 +0900

    Update PostHeader.tsx

--- src/containers/PostDetail/components/PostHeader.tsx
+++ src/containers/PostDetail/components/PostHeader.tsx
@@ -21,7 +21,7 @@ const PostHeader: React.FC<Props> = ({ data }) => {
           <div className="flex items-center gap-4 mb-3">
             {data.author && data.author[0] && (
               <div className="flex items-center gap-2">
-                {data.author[0]?.profile_photo ? (<Image
+                {data.author[0]?.profile_photo?.startWith?.('http') ? (<Image
                     className="rounded-full"
                     src={data.author[0].profile_photo}
                     alt="profile_photo"
